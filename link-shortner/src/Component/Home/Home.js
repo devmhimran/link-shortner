b2765dbd53a26ae0ac81f57308cb75971ef49621
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-06-27 04:29:24 +0600

    Added API & Update Background

--- link-shortner/src/Component/Home/Home.js
+++ link-shortner/src/Component/Home/Home.js
@@ -1,29 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
+    const [shortenLink, setShortenLink] = useState('');
+    // console.log(shortenLink.result.short_link3);
     const handleLink = (e) => {
         e.preventDefault();
         const link = e.target.inputLink.value;
-        console.log(link);
+
+        fetch(`https://api.shrtco.de/v2/shorten?url=${link}`, {
+            method: 'POST'
+        })
+            .then(res => res.json())
+            .then(des => setShortenLink(des))
+
+        console.log(shortenLink);
+    }
+    const handleCopyLink = (e) => {
+
     }
     return (
-        <div className='container mx-auto w-full h-screen flex items-center'>
-            <div className="card shadow-xl w-3/5 mx-auto">
+        <div className='link__shortner__container container mx-auto w-full h-screen flex items-center'>
+            <div className="card shadow-xl w-3/5 mx-auto rounded-none">
                 <div className="card-body">
-                    <h1 className='text-3xl font-semibold'>Link Shortner</h1>
+                    <h1 className='text-3xl font-semibold text-white'>Link Shortner</h1>
                     <form onSubmit={handleLink}>
                         <div className="form-control">
-                            <label className="input-group my-2">
-                                <input name='inputLink' type="text" placeholder="Enter Your Link" className="input input-bordered w-full" />
+                            <label className="input-group my-2 rounded-none">
+                                <input name='inputLink' type="text" placeholder="Enter Your Link" className="link_input input input-bordered text-white w-full" />
                                 <button className='btn btn-primary'>Shorten</button>
                             </label>
-                            <label className="input-group my-2">
-                                <input type="text" placeholder="Enter Your Link" className="input input-bordered w-full" />
-                                <button className='btn btn-primary'>Copy to clipboard</button>
-                            </label>
+
                         </div>
                     </form>
+                    <>
+                    {
+                        shortenLink &&
+                        (<form onSubmit={handleCopyLink}>
+                            <div className="form-control">
+                                <label className="input-group my-2 rounded-none justify-center">
+                                    <input type="text" placeholder="Enter Your Link" defaultValue={shortenLink.result.short_link3} className="link_output input input-bordered text-white w-1/2 " />
+                                    <button className='btn btn-primary'>Copy to clipboard</button>
+                                </label>
+                            </div>
+                        </form>)
+                    }
+                    </>
                 </div>
             </div>
         </div>
